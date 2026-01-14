b44bb58c643c3caa1f5bc879ccdb2c88429c9cf0
xujunran <xujunran@outlook.com>
2026-01-14 11:29:20 +0800

    building abstractions with procuduresa

--- 1-building-abstractions-with-procudures.scm
+++ 1-building-abstractions-with-procudures.scm
@@ -24,8 +24,8 @@
    (+ (- 10 7)
       6))
 
-;; naming
-;; (define <name> <value>)
+;;; naming
+;;  (define <name> <e>)
 
 (define size 2)
 size
@@ -38,6 +38,85 @@ size
 (define circumference (* 2 pi radius))
 circumference ; 注意：这不是过程，加括号就错了
 
-;; procedure definition
-;; (define (<name> <formal parameters>) <body>)
+;;; procedure definition
+;;  (define (<name> <formal parameters>) <body>)
+
+(define (square x) (* x x))
+(square 21)
+(square (+ 2 5))
+(square (square 3))
+
+(define (sum-of-square x y)
+  (+ (square x) (square y)))
+
+(sum-of-square 3 4)
+
+(define (f a)
+  (sum-of-square (+ a 1) (* a 2)))
+
+(f 5)
+
+;;; conditional expression
+;;  (cond (<p1> <e1>)
+;;        (<p2> <e2>)
+;;        .
+;;        .
+;;        .
+;;        (<pn> <en>)
+
+;;  (cond (<p1> <e1>)
+;;        (<p2> <e2>)
+;;        .
+;;        .
+;;        .
+;;        (<pn> <en>)
+;;        (else <e>)
+
+;;  (if <predicate> <consequent> <alternative>)
+
+;;  (if <p> <e1> <e2>)
+
+(define (abs x)
+  (cond ((> x 0) x)
+	((= x 0) 0)
+	((< x 0) (- x))))
+
+(abs 0)
+(abs 1)
+(abs -1)
+
+;;; boolean
+true
+false
+
+#t
+#f
+
+;;; logical
+;;  (and <e1> ... <en>)
+;;  (or <e1> ... <en>)
+;;  (and <e>)
+
+(define x 1)
+(and (> x 5) (< x 10))
+
+(define x 7)
+(and (> x 5) (< x 10))
+
+(define x 12)
+(and (> x 5) (< x 10))
+
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+(define (>=2 x y)
+  (not (< x y)))
+
+(>= 1 1)
+(>= 1 0)
+(>= 1 2)
+
+(>=2 1 1)
+(>=2 1 0)
+(>=2 1 2)
 
