ec8d5eabc1a8a11e2a8430ad55df2b256b8915d7
xujunran <xujunran@outlook.com>
2026-01-14 23:27:55 +0800

    exercise1.8

---
+++ exercise1.8.scm
@@ -0,0 +1,22 @@
+(define (cube-root x)
+  (define (iter guess x)
+    (if (good-enough? guess (improve guess x))
+	(improve guess x)
+	(iter (improve guess x)
+	      x)))
+  (define (good-enough? pre-guess guess)
+    (> 0.01
+       (abs (/ (- guess pre-guess)
+	       pre-guess))))
+  (define (improve guess x)
+    (/ (+ (/ x
+	     (* guess guess))
+	  (* 2 guess))
+       3))
+  (iter 1.0 x))
+
+(cube-root (* 2 2 2))
+(cube-root (* 3 3 3))
+(cube-root (* 4 4 4))
+
+
